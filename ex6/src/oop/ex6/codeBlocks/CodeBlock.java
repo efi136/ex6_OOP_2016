aff8ca9b6a269e277b30bd7d2364a5f2745cb5e1
t8281255 <t8281255@tlp-t-301.talpiot.huji.ac.il>
2016-06-13 20:31:19 +0300

    Finished the method getVarNamesFromConditions

--- ex6/src/oop/ex6/codeBlocks/CodeBlock.java
+++ ex6/src/oop/ex6/codeBlocks/CodeBlock.java
@@ -1,5 +1,7 @@
 package oop.ex6.codeBlocks;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import oop.ex6.main.FileParser;
 import oop.ex6.variables.BooleanVariable;
 import oop.ex6.variables.DoubleVariable;
@@ -11,6 +13,7 @@ public class CodeBlock {
 			DoubleVariable.VALUE_REGEX+"|"+IntVariable.VALUE_REGEX+"|"+Variable.NAME_REGEX+")";
 	public static final String COMPLEX_COND = "(\\s*" + COND + "(\\s*([&]{2}|[|]{2})\\s*" + COND + ")*)";
 	public static final String BLOCK_END = "}";
+	public static final String CONDITION_VARIABLE = "([&]{2}|[|]{2})";
 	
 	/**
 	 * This function gets a condition string and returns all of the variables\from this condition.
@@ -18,8 +21,18 @@ public class CodeBlock {
 	 * @return
 	 */
 	protected static String[] getVariableNamesFromCondition(String condition){
-		return new String[1];
-		// TODO :: do this.
+		String minimized = condition.substring(condition.indexOf('('), condition.indexOf(')')); //TODO: check if this includes ( and )
+		Pattern p = Pattern.compile(CONDITION_VARIABLE);
+		Matcher m = p.matcher(minimized);
+		int amount = (minimized.length() - minimized.replace("||", "").replace("&&", "").length())/2;
+		String[] variables = new String[amount];
+		for (int i = 0; i < amount-1; i++){
+			m.find();
+			variables[i] = minimized.substring(0, m.start()).replace(" ", "");
+			minimized = minimized.substring(m.end());
+		}
+		variables[amount-1] = minimized.replace(" ", "");
+		return variables;
 	}
 	
 	public boolean compile(FileParser parser){
