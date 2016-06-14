3edf39b334d1430205c200e5a78d3f91b2584bd7
t8281255 <t8281255@tlp-t-301.talpiot.huji.ac.il>
2016-06-14 08:11:54 +0300

    Some more comments

--- ex6/src/oop/ex6/variables/BooleanVariable.java
+++ ex6/src/oop/ex6/variables/BooleanVariable.java
@@ -11,19 +11,33 @@ import oop.ex6.Exceptions.UsedBeforeAssignment;
 public class BooleanVariable extends Variable {
 	@SuppressWarnings("unused")
 	private boolean value;
-	
+	/**
+	 * Regex expressions for a boolean variable
+	 */
 	public static final String VALUE_REGEX = "(true|false|"+DoubleVariable.VALUE_REGEX+")";
 	public static final String ASSIGNMENT = Variable.NAME_REGEX + "(\\s*=\\s*("+VALUE_REGEX+"|" + Variable.NAME_REGEX + "))?";
 	public static final String ASSIGNMENT_LINE = Variable.NAME_REGEX + "(\\s*=\\s*("+VALUE_REGEX+"|" + Variable.NAME_REGEX +"));";
 	public static final String TYPE = "boolean";
 	public static final String DECLERATION = "(final \\s*)?" +  TYPE+"\\s*"+ASSIGNMENT+"(\\s*,\\s*"+ASSIGNMENT+")*;";
 
+	/**
+	 * Checks if a line is a boolean variable decleration
+	 * @param line - The line to check
+	 * @return - True if it's a boolean variable decleration and false otherwise
+	 */
 	public static boolean isBooleanVariableDec(String line){
 		Pattern p = Pattern.compile(DECLERATION);
 		Matcher m = p.matcher(line);
 		return m.matches();
 	}
 	
+	/**
+	 * Returns all the variables declared in one line.
+	 * @param line - The line to check.
+	 * @param st - The symbol table.
+	 * @return - an array of boolean variables.
+	 * @throws Ex6Exceptions - A general exception.
+	 */
 	public static BooleanVariable[] getVariablesFromDec(String line, SymbolTable st) throws Ex6Exceptions{
 		int count = line.length() - line.replace(",", "").length();
 		count = count+1;
@@ -40,6 +54,15 @@ public class BooleanVariable extends Variable {
 		return vars;
 	}
 	
+	/**
+	 * Returns the variable declared in a part of a line.
+	 * @param line - The line.
+	 * @param start_index - The index in the line where the variable starts.
+	 * @param fin - Is the variable final or not
+	 * @param st - The symbol table.
+	 * @return a variable.
+	 * @throws Ex6Exceptions - General exception.
+	 */
 	public static BooleanVariable getVariableFromLinePart(String line, int[] start_index, boolean fin, SymbolTable st) throws Ex6Exceptions{
 		Pattern pattern = Pattern.compile(ASSIGNMENT);
 		Matcher matcher = pattern.matcher(line);
@@ -55,7 +78,7 @@ public class BooleanVariable extends Variable {
 				return new BooleanVariable(name, Boolean.getBoolean(matcher.group()), fin);
 			}
 			matcher.usePattern(Pattern.compile(Variable.NAME_REGEX));
-			if(matcher.find()){
+			if(matcher.find()){ //If it's an asignment of a variable in a variable.
 				String secondVariableName = matcher.group();
 				if(st.isInit(secondVariableName)){
 					if(st.get_variable_type(secondVariableName).equals(TYPE)){
@@ -71,8 +94,9 @@ public class BooleanVariable extends Variable {
 					throw new UsedBeforeAssignment(name);
 				}
 			}
-			else{
+			else{ //If un initialized
 				if(fin){
+					//Uninitialized final
 					throw new UnInitializedFinal(name);
 				}
 				return new BooleanVariable(name);
